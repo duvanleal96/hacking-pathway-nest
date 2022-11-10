9fe7b001a3427872e36682a1a8bb54224c2c0178
duvan leal <duvanleal65@gmail.com>
2022-11-09 20:39:55 -0500

    finally

--- src/invoice/services/invoice.service.ts
+++ src/invoice/services/invoice.service.ts
@@ -40,7 +40,55 @@ export class InvoiceService {
     },
   ];
 
-  getInvoices(): InvoiceDto[]{
+  getInvoices(): InvoiceDto[] {
     return this.invoice;
   }
+
+  createIvoice(invoice: InvoiceDto): InvoiceDto {
+    this.invoice.push(invoice);
+    return invoice;
+  }
+
+  getById(uuid: string): InvoiceDto | undefined {
+    return this.invoice.find((invoice: InvoiceDto) => (invoice.uuid = uuid));
+  }
+  putInvoices(uuid: string, invoice: InvoiceDto): InvoiceDto | undefined {
+    const invoices = this.invoice.find(
+      (invoice: InvoiceDto) => (invoice.uuid = uuid),
+    );
+    if (invoices != undefined) {
+      invoices.customerUuid = invoice.customerUuid;
+      invoices.nit = invoice.nit;
+      invoices.invoiceDetail = invoice.invoiceDetail;
+    }
+    return invoices;
+  }
+  pathInvoices(
+    uuid: string,
+    invoiceUpdate: InvoiceDto,
+  ): InvoiceDto | undefined {
+    const invoice = this.invoice.find(
+      (invoice: InvoiceDto) => (invoice.uuid = uuid),
+    );
+    if (invoice != undefined) {
+      const invoicePath: InvoiceDto = {
+        ...invoice,
+        ...invoiceUpdate,
+      };
+      this.invoice = this.invoice.map((invoice: InvoiceDto) => {
+        return invoice.uuid == uuid ? invoicePath : invoice;
+      });
+      return invoicePath;
+    }
+    return invoice;
+  }
+
+  deleteInvoice(uuid: string): boolean {
+    const deleteInvoice = this.invoice.find(
+      (invoice: InvoiceDto) => (invoice.uuid = uuid),
+    );
+    if (deleteInvoice) return true;
+    return false;
+  }
+
 }
